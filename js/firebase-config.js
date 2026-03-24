9abfbdf15c89ed5ada071ec27ea8d6a555a53ce7
Gean <jdioses@outlook.be>
2026-03-24 22:51:11 +0100

    Hide Firebase config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,19 @@
-// Este archivo contiene la configuración de tu proyecto de Firebase.
-// ¡IMPORTANTE! Si usas un sistema de control de versiones como Git,
-// añade este archivo a tu .gitignore para no exponer tus claves.
+const firebaseEnv = globalThis.__APPLEO_FIREBASE_CONFIG__ || {};
+
+function required(name, value) {
+  if (!value) {
+    throw new Error(`Missing required Firebase config: ${name}`);
+  }
+
+  return value;
+}
 
 export const FIREBASE_CONFIG = Object.freeze({
-  apiKey: "AIzaSyCRvodMEsVaZ0ynCqTTR8quIAAvW445kzE",
-  authDomain: "appleo-a0ba4.firebaseapp.com",
-  projectId: "appleo-a0ba4",
-  storageBucket: "appleo-a0ba4.firebasestorage.app",
-  messagingSenderId: "1045704718169",
-  appId: "1:1045704718169:web:c422f19c13176efae6be48",
-  measurementId: "G-BG54P5T72H",
+  apiKey: required('apiKey', firebaseEnv.apiKey),
+  authDomain: required('authDomain', firebaseEnv.authDomain),
+  projectId: required('projectId', firebaseEnv.projectId),
+  storageBucket: required('storageBucket', firebaseEnv.storageBucket),
+  messagingSenderId: required('messagingSenderId', firebaseEnv.messagingSenderId),
+  appId: required('appId', firebaseEnv.appId),
+  measurementId: required('measurementId', firebaseEnv.measurementId),
 });
